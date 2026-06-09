021daca2923bfecc6102b449225cb555e5ad42e3
Tusiime Parvin Ruth <parvin.t.ruth@gmail.com>
2026-06-09 18:14:12 +0300

    Edited some files

---
+++ server.js
@@ -0,0 +1,12 @@
+//1.Dependencies
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const expressSession = require("express-session");
+const passport = require ("passport");
+
+//Import registration model
+require ("dotenv").config();
+//Import routes
+
+//2.Installations
